b2b480369fcb753cb7340f4cd62e71b2d16146ad
Shivu B <sbhat592@gmail.com>
2024-02-11 02:40:38 -0600

    uddate

--- main.js
+++ main.js
@@ -13,13 +13,11 @@ document.addEventListener("DOMContentLoaded", async function () {
   const dataUrl = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vRhBfkLZwlSmj2Rh0w8AFLlirlzCm_26qZnf4tIcE5e8qgqQz7NtFBZyhBRX61TB0-jCignTKJNdOty/pub?gid=0&single=true&output=tsv';
   const moment = window.moment || (await import('https://cdn.jsdelivr.net/momentjs/latest/moment.min.js')).default;
 
-
   let tableData = [];
   let selectedColumnIndex = -1;
   let currentDate = moment().startOf('day');
   let currentBlockStartDate;
   let currentBlockEndDate;
-
   let schoolYearStart = getFirstSundayInJuly(specificYear);
   const daysElapsed = Math.ceil(currentDate.diff(schoolYearStart, "days"));
   let blockNumber = Math.ceil(daysElapsed / 28);
@@ -43,7 +41,6 @@ document.addEventListener("DOMContentLoaded", async function () {
     const dayOfWeek = julyMoment.day(); // Calculate the day of the week for July 1st
     const daysToAdd = dayOfWeek === 0 ? 0 : 7 - dayOfWeek; // Calculate the number of days to add to reach the first Sunday
     const firstSundayMoment = julyMoment.add(daysToAdd, 'days').startOf('day'); // Add the days to get the first Sunday
-
     return firstSundayMoment;
   }
 
@@ -51,24 +48,23 @@ document.addEventListener("DOMContentLoaded", async function () {
   // ************************ Initializes the original table ************************************************
   async function initializeTable() {
     tableData = await getTableData();
-        // Log the schoolYearStart and currentDate for debugging
+    // Log the schoolYearStart and currentDate for debugging
     console.log('schoolYearStart:', schoolYearStart.format('YYYY-MM-DD'));
     console.log('currentDate:', currentDate.format('YYYY-MM-DD'));
     console.log('Days Elapsed:', daysElapsed);
-       // Resets all variables if its July 1st
+    // Resets all variables if its July 1st
     if (currentDate.isSame(schoolYearStart.clone().startOf('year').add(6, 'months'), 'day')) {
       schoolYearStart = getFirstSundayInJuly(currentDate.year());
       currentDate = moment().startOf('day');
       daysElapsed = Math.ceil(currentDate.diff(schoolYearStart, "days"));
       blockNumber = 1;
     } else {
-          // Calculates the block number 
-      blockNumber = Math.ceil(daysElapsed / 28);
+      blockNumber = Math.ceil(daysElapsed / 28); // Calculates the block number 
     }
-        // Current block's start and end dates based on the current block number
+    // Current block's start and end dates based on the current block number
     currentBlockStartDate = schoolYearStart.clone().add((blockNumber - 1) * 28, "days");
     currentBlockEndDate = currentBlockStartDate.clone().add(27, "days");
-       // Check if today's date is within the current block, otherwise it moves to the next block
+    // Check if today's date is within the current block, otherwise it moves to the next block
     if (!(currentDate.isSameOrAfter(currentBlockStartDate) && currentDate.isSameOrBefore(currentBlockEndDate))) {
       moveToNextBlock();
     }
@@ -89,13 +85,13 @@ document.addEventListener("DOMContentLoaded", async function () {
   function updateDates() {
     currentBlockStartDate = schoolYearStart.clone().add((blockNumber - 1) * 28, "days");
     currentBlockEndDate = currentBlockStartDate.clone().add(27, "days");
-       //Consoles for debugging
+    //Consoles for debugging
     console.log('schoolYearStart:', schoolYearStart.format('YYYY-MM-DD'));
     console.log('currentDate:', currentDate.format('YYYY-MM-DD'));
     console.log('blockNumber:', blockNumber);
     console.log('currentBlockStartDate:', currentBlockStartDate);
     console.log('currentBlockEndDate:', currentBlockEndDate);
-    
+
     theadElement.appendChild(createTableHeader(tableData[0]));
     tableElement.appendChild(theadElement);
     tableElement.appendChild(tbodyElement);
@@ -108,24 +104,22 @@ document.addEventListener("DOMContentLoaded", async function () {
   function createTableHeader(data) {
     const trElement = document.createElement("tr");
     trElement.setAttribute("class", "head-row");
-          // Always display Date and Day columns
+    // Always display Date and Day columns
     ['DATE', 'DAY'].forEach((item, index) => {
       const thElement = document.createElement("th");
       thElement.setAttribute("class", `head-cell${index === 0 || index === 1 ? ' special-column' : ''}`);
       thElement.innerHTML = `<span>${item}</span>`;
       trElement.appendChild(thElement);
     });
-         // Rest of the columns
+    // Rest of the columns
     data.slice(2).forEach((item) => {
       const thElement = document.createElement("th");
       thElement.setAttribute("class", "head-cell");
       thElement.innerHTML = `<span>${item}</span>`;
       trElement.appendChild(thElement);
     });
-
     return trElement;
   }
-
 
   // ************************ Update the title based on the current block ************************************************
   function updateTitle() {
@@ -138,28 +132,25 @@ document.addEventListener("DOMContentLoaded", async function () {
   function createTrForTableBody(data) {
     const trElement = document.createElement("tr");
     trElement.setAttribute("class", "body-row");
-         // Check if the date is today
+    // Check if the date is today
     const date = moment(data[0]).toDate();
     if (isToday(date)) {
       trElement.classList.add("body-row--today");
     }
-        //DATE CELL 
+    //DATE CELL 
     const dateValue = data[0];
     const dateCell = document.createElement("td");
     dateCell.setAttribute("class", `body-cell${' special-column'}`);
-
-       // Format the date using moment.js
+    // Format the date using moment.js
     const formattedDate = moment(dateValue).format('MMM D');
     dateCell.innerHTML = `<span>${formattedDate}</span>`;
     trElement.appendChild(dateCell);
-
-       // Create cell for Day using the value from Google Sheets
+    // Create cell for Day using the value from Google Sheets
     const dayCell = document.createElement("td");
     dayCell.setAttribute("class", "body-cell special-column");
     const dayValue = data[1]; // Assuming the "Day" column is at index 1
     dayCell.innerHTML = `<span>${dayValue}</span>`;
     trElement.appendChild(dayCell);
-
     // Create cells based on selected column
     if (selectedColumnIndex !== -1) {
       const tdElement = document.createElement("td");
@@ -181,7 +172,6 @@ document.addEventListener("DOMContentLoaded", async function () {
   // ************************ Update the table based on the selected filters ************************************************
   function updateTable() {
     tbodyElement.innerHTML = '';
-
     if (filterDropdown.value === "") {
       // Display the original table header
       theadElement.innerHTML = '';
@@ -211,7 +201,6 @@ document.addEventListener("DOMContentLoaded", async function () {
       // Update header
       theadElement.innerHTML = '';
       theadElement.appendChild(createTableHeader(headerData));
-
       // Iterate through the table data and update the rows
       for (let i = 1; i < tableData.length; i++) {
         const trElementForBody = createTrForTableBody(tableData[i]);
@@ -234,7 +223,6 @@ document.addEventListener("DOMContentLoaded", async function () {
     highlightCells();
   }
 
-
   // Highlight cells based on search input
   function highlightCells() {
     const searchTerm = searchInput.value.trim().toLowerCase();
@@ -256,13 +244,11 @@ document.addEventListener("DOMContentLoaded", async function () {
   function updateFilterDropdown(data) {
     // Clear existing options
     filterDropdown.innerHTML = '';
-
     // Create a default "Select a Team" option
     const defaultOption = document.createElement("option");
     defaultOption.value = "";
     defaultOption.textContent = "Select a Team";
     filterDropdown.appendChild(defaultOption);
-
     // Add options for each column
     data.slice(2).forEach((item, index) => {
       const optionElement = document.createElement("option");
@@ -272,13 +258,11 @@ document.addEventListener("DOMContentLoaded", async function () {
     });
   }
 
-  // Function to move to the next block
+  // Function to move blocks
   function moveToNextBlock() {
     blockNumber++;
     updateDates();
   }
-
-  // Function to move to the previous block
   function moveToPreviousBlock() {
     if (blockNumber > 1) {
       blockNumber--;
@@ -292,35 +276,28 @@ document.addEventListener("DOMContentLoaded", async function () {
     updateTable();
     updateTitle();
   });
-
   previousWeeksButton.addEventListener("click", () => {
     moveToPreviousBlock();
     updateTable();
     updateTitle();
   });
-
   searchInput.addEventListener("input", function () {
     updateTable();
   });
-
   filterDropdown.addEventListener("change", function () {
     selectedColumnIndex = parseInt(filterDropdown.value);
     updateTable();
   });
-
-  
-// Event listeners for buttons and input fields
-resetButton.addEventListener("click", function () {
-  selectedColumnIndex = -1;
-  searchInput.value = "";
-  filterDropdown.value = "";
-  initializeTable(); // Reinitialize the table
-});
+  resetButton.addEventListener("click", function () {
+    selectedColumnIndex = -1;
+    searchInput.value = "";
+    filterDropdown.value = "";
+    initializeTable(); // Reinitialize the table
+  });
 
   // Hover effect for rows and columns
   tableElement.addEventListener("mouseover", handleHover);
   tableElement.addEventListener("mouseout", handleHover);
-
   function handleHover(event) {
     const target = event.target;
     const cell = target.closest("th, td");
